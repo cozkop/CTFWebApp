e639f5090f56f11920da0acd6537e72a77c743f1
Corey Kopotsha <mrcoreykopotsha@outlook.com>
2019-11-17 14:37:21 +0000

    AddIdentityViews

---
+++ CTFWebApp/Models/ApplicationRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTFWebApp.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+
+        public ApplicationRole(string roleName) : base(roleName) { }
+    }
+}
